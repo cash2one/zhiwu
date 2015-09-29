9067744df317101e31a861168c90036f80281c2e
dodoleon <gaotaohu@163.com>
2015-09-29 19:52:33 +0800

    url

--- zhiwuzufang/urls.py
+++ zhiwuzufang/urls.py
@@ -19,21 +19,17 @@ from django.contrib import admin
 from django.conf import settings
 
 urlpatterns = [
-
-    url(r'^work_search/', 'zhiwu.view.worksearch',name='work_search'),
-    url(r'^home_search/', 'zhiwu.view.homesearch',name='home_search'),
-
-    # url(r'^room_search/', 'zhiwu.view.roomsearch',name='room_search'),
-
-    url(r'^room_detail/', 'zhiwu.view.roomdetail',name='room_detail'),
-    url(r'^manager_login/', 'zhiwu.view.manager_login',name='manager_login'),
-    url(r'^admin_root/', 'zhiwu.view.admin_root',name='admin_root'),
-    url(r'^admin_manager/', 'zhiwu.view.admin_manager',name='admin_manager'),
+    url(r'^$', 'zhiwu.views.home', name='home'),
+    url(r'^work_search/', 'zhiwu.views.work_search', name='work_search'),
+    url(r'^home_search/', 'zhiwu.views.home_search', name='home_search'),
+    url(r'^room_detail/', 'zhiwu.views.room_detail', name='room_detail'),
+    url(r'^manager_login/', 'zhiwu.views.manager_login',name='manager_login'),
+    url(r'^admin_root/', 'zhiwu.views.admin_root',name='admin_root'),
+    url(r'^admin_manager/', 'zhiwu.views.admin_manager',name='admin_manager'),
     # root get user
-    url(r'^admin_second_manager/', 'zhiwu.view.admin_second_manager',name='admin_second_manager'),
+    url(r'^admin_second_manager/', 'zhiwu.views.admin_second_manager',name='admin_second_manager'),
 
     # url(r'^insert/','zhiwu.views.insert', name='insert'),
-    url(r'^$', 'zhiwu.views.home', name='home'),
     # url(r'^search/', 'zhiwu.views.search', name='search'),
     # url(r'^room_detail/', 'zhiwu.views.room_detail', name='room_detail'),
     # url(r'^admin_login/', 'zhiwu.views.admin_login', name='admin_login'),
@@ -42,40 +38,34 @@ urlpatterns = [
     # url(r'^admin_uploader/', 'zhiwu.views.admin_uploader', name='admin_uploader'),
 
     # ajax
-    url(r'^map_search/', 'zhiwu.view.mapsearch', name='map_search'), 
+    url(r'^map_search/', 'zhiwu.views.mapsearch', name='map_search'),
     # 地图拖动接口 data:longtitude,latitude
-    url(r'^room_collection/', 'zhiwu.view.roomcollection', name='room_collection'), 
+    url(r'^room_collection/', 'zhiwu.views.roomcollection', name='room_collection'),
     # 收藏房源 data:room_num
     # post
-    url(r'^manager_add/', 'zhiwu.view.manageradd', name='manager_add'),
-    url(r'^manager_delete/', 'zhiwu.view.manageradd', name='manager_delete'),
-    url(r'^manager_modify/', 'zhiwu.view.managermodify', name='manager_modify'), 
-    url(r'^manager_logout/', 'zhiwu.view.managerlogout', name='manager_logout'), 
-    url(r'^manager_active/', 'zhiwu.view.manageractive', name='manager_active'), 
-    url(r'^manager_pw/', 'zhiwu.view.managerpw', name='manager_pw'), 
+    url(r'^manager_add/', 'zhiwu.views.manageradd', name='manager_add'),
+    url(r'^manager_delete/', 'zhiwu.views.manageradd', name='manager_delete'),
+    url(r'^manager_modify/', 'zhiwu.views.managermodify', name='manager_modify'),
+    url(r'^manager_logout/', 'zhiwu.views.managerlogout', name='manager_logout'),
+    url(r'^manager_active/', 'zhiwu.views.manageractive', name='manager_active'),
+    url(r'^manager_pw/', 'zhiwu.views.managerpw', name='manager_pw'),
 
-    url(r'^second_manager_add/', 'zhiwu.view.secondmanageradd', name='second_manager_add'),
-    url(r'^second_manager_modify/', 'zhiwu.view.secondmanagermodify', name='second_manager_modify'), 
-    url(r'^second_manager_logout/', 'zhiwu.view.secondmanagerlogout', name='second_manager_logout'), 
-    url(r'^second_manager_active/', 'zhiwu.view.secondmanageractive', name='second_manager_active'), 
-    url(r'^second_manager_pw/', 'zhiwu.view.secondmanagerpw', name='second_manager_pw'),
+    url(r'^second_manager_add/', 'zhiwu.views.secondmanageradd', name='second_manager_add'),
+    url(r'^second_manager_modify/', 'zhiwu.views.secondmanagermodify', name='second_manager_modify'),
+    url(r'^second_manager_logout/', 'zhiwu.views.secondmanagerlogout', name='second_manager_logout'),
+    url(r'^second_manager_active/', 'zhiwu.views.secondmanageractive', name='second_manager_active'),
+    url(r'^second_manager_pw/', 'zhiwu.views.secondmanagerpw', name='second_manager_pw'),
 
-    url(r'^room_logout/', 'zhiwu.view.roomlogout', name='room_logout'),
-    url(r'^room_active/', 'zhiwu.view.roomactive', name='room_active'),
-    url(r'^room_save/', 'zhiwu.view.roomsave', name='room_save'),
-    url(r'^room_sub/', 'zhiwu.view.roomsub', name='room_sub'),
-    url(r'^room_sold/', 'zhiwu.view.roomsold', name='room_sold'),
-
-
-
-
-
-
-
+    url(r'^room_logout/', 'zhiwu.views.roomlogout', name='room_logout'),
+    url(r'^room_active/', 'zhiwu.views.roomactive', name='room_active'),
+    url(r'^room_save/', 'zhiwu.views.roomsave', name='room_save'),
+    url(r'^room_sub/', 'zhiwu.views.roomsub', name='room_sub'),
+    url(r'^room_sold/', 'zhiwu.views.roomsold', name='room_sold'),
 
     url(r'^admin/', include(admin.site.urls)),
     # url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_URL}),
     #
+    url(r'^js/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_URL+"js"}),
     url(r'^stylesheet/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_URL+"stylesheet"}),
     url(r'^resource/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_URL+"resource"}),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
